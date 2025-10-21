32d2cd430f1ad7eb80d55f8c66502607708ab9b2
Saeeda <saeedabatool39@gmail.com>
2025-10-21 17:17:51 -0400

    rerun

--- ansible/scripts/secrets_script.sh
+++ ansible/scripts/secrets_script.sh
@@ -2,7 +2,7 @@
 set -e
 
 aws secretsmanager get-secret-value \
-    --secret-id ansible/vault/my-portfolio/vault_password \
+    --secret-id ansible/vault/password \
     --region us-east-1 \
     --query SecretString --output text
 
